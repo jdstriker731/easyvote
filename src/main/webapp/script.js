af2433a570202289d01074911f7b7fc9552536e6
myles <mbostic32@gmail.com>
2020-06-24 20:18:31 +0000

    whitespace!

--- src/main/webapp/script.js
+++ src/main/webapp/script.js
@@ -12,21 +12,21 @@
 // See the License for the specific language governing permissions and
 // limitations under the License
 function requestTranslation() {
-    const text = document.getElementById('text').value;
-    const languageCode = document.getElementById('language').value;
+  const text = document.getElementById('text').value;
+  const languageCode = document.getElementById('language').value;
 
-    const resultContainer = document.getElementById('result');
-    resultContainer.innerText = 'Loading...';
+  const resultContainer = document.getElementById('result');
+  resultContainer.innerText = 'Loading...';
 
-    const params = new URLSearchParams();
-    params.append('text', text);
-    params.append('languageCode', languageCode);
-
-    fetch('/translate', {
-      method: 'POST',
-      body: params
-    }).then(response => response.text())
-    .then((translatedMessage) => {
-      resultContainer.innerText = translatedMessage;
-    });
+  const params = new URLSearchParams();
+  params.append('text', text);
+  params.append('languageCode', languageCode);
+  
+  fetch('/translate', {
+    method: 'POST',
+    body: params
+  }).then(response => response.text())
+  .then((translatedMessage) => {
+    resultContainer.innerText = translatedMessage;
+  });
 }
